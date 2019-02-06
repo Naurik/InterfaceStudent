85879ad52372d182e414cc07c10b40917f5b6cd2
Nauryzbai <nauzik@mail.ru>
2019-02-06 15:24:15 +0600

    Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Stud("Aibar", 200500, 5, 89);
+            Console.WriteLine("Имя студента: " + student.GetName);
+            Console.WriteLine("Плата за обучение: " + student.GetPaymentStudy);
+            Console.WriteLine("Срок обучения: " + student.GetYearStudy);
+            Console.WriteLine("Рейтинг студента: " + student.GetRating);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interface/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class Student : IStudent
+    {
+        private string _name;
+        private int _money;
+        private int _yearsLearn;
+        private int _ratings;
+
+        public Student() { }
+        public void Stud(string name, int money, int yearsLearn, int ratings)
+        {
+            _name = name;
+            _money = money;
+            _yearsLearn = yearsLearn;
+            _ratings = ratings;
+        }
+        public void Name(string name)
+        {
+            _name = name;
+        }
+
+        #region Interface
+        public string GetName { get { return _name; } }
+        public int GetPaymentStudy { get { return _money; } }
+        public int GetYearStudy { get { return _yearsLearn; } }
+        public int GetRating { get { return _ratings; } }
+        #endregion
+    }
+}
